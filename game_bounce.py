0f7ca96ea193f1ea7944276c188517191a3f2fff
Denis Polyakov <3dsdenis@gmail.com>
2022-11-12 12:23:16 +0300

    Создание площадки для отбивания меча

--- game_bounce.py
+++ game_bounce.py
@@ -26,6 +26,32 @@ class Ball: # Мяч
 		if pos[2] >= self.canvas_width:
 			self.x = -3
 
+class Paddle:
+	def __init__(self, canvas, color):
+		self.canvas = canvas
+		self.id = canvas.create_rectangle(0, 0 , 100, 10, fill=color)
+		self.canvas.move(self.id, 200, 300)
+		self.x = 0
+		self.canvas_width = self.canvas.winfo_width()
+		self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+		self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+
+	def draw(self):
+		self.canvas.move(self.id, self.x, 0)
+		pos = self.canvas.coords(self.id)
+		if pos[0] <= 0:
+			self.x = 0
+
+		elif pos[2] >= self.canvas_width:
+			self.x = 0
+
+	def turn_left(self, evt):
+		self.x = -2
+
+	def turn_right(self, evt):
+		self.x = 2
+
 
 
 
@@ -41,10 +67,12 @@ canvas = Canvas(tk, width=500, height=400,bd=0, highlightthickness=0)
 canvas.pack() #При вызове холст изменит размер в соответствии со значениями ширины и высоты
 tk.update()
 
+paddle = Paddle(canvas, "blue")
 ball = Ball(canvas, 'red')
 
 while 1: #Главный цикл программы
 	ball.draw()
+	paddle.draw()
 	tk.update_idletasks()
 	tk.update()
 	time.sleep(0.01)
